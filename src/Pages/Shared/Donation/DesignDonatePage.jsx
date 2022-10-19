12f5c1545b11b717e75e98ff5b0b65d885990e9a
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-10-19 12:31:33 +0600

    respons on design donation pages

--- src/Pages/Shared/Donation/DesignDonatePage.jsx
+++ src/Pages/Shared/Donation/DesignDonatePage.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 const DesignDonatePage = () => {
   return (
-    <section className="mx-20 mb-10 ">
+    <section className="mx-10 md:mx-20 mb-10 ">
       <Link to={"/donation"}>
-        <div className="hero bg-[#EBF1F6] p-10">
+        <div className="hero bg-[#EBF1F6] md:p-10">
           <div className="hero-content flex-col lg:flex-row">
             <img
               src="https://i.ibb.co/6Y6rVBF/child.png"
               className=" rounded-lg shadow-2xl"
               alt="img"
             />
-            <div className="ml-20">
-              <h1 className="text-4xl font-bold">
+            <div className="md:ml-20">
+              <h1 className="text-2xl md:text-4xl font-bold">
                 Help a Child in Greatest Need
               </h1>
               <p className="py-6 text-justify">
